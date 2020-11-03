874cf532c071d2381b136ff32ca42870076b0fa5
HappyFeet <jbulldogs55@gmail.com>
2020-11-03 10:16:11 -0600

    Update GroupProject.m

--- GroupProject.m
+++ GroupProject.m
@@ -1,2 +1,5 @@
+clear 
 clc
-clear
+materials = {'Concrete','Wood','Adobe'};
+Menu = menu('Select Material', materials);
+array1 = {'Concrete', 16, 30,96000,900,5,5;'Wood',23,53,115000,800,12,11;'Adobe',18,42,68000,600,6,5};
