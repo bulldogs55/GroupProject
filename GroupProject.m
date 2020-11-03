6444d671f346ccc8e1420d8b496d858efeb4af52
HappyFeet <jbulldogs55@gmail.com>
2020-11-03 10:27:46 -0600

    Update GroupProject.m

--- GroupProject.m
+++ GroupProject.m
@@ -4,4 +4,6 @@ materials = {'Concrete','Wood','Adobe'};
 Menu = menu('Select Material', materials);
 array1 = {'Concrete', 16, 30,96000,900,5,5;'Wood',23,53,115000,800,12,11;'Adobe',18,42,68000,600,6,5};
 
-chosenArray = array1{Menu,:}
+chosenArray = array1(Menu,:)
+
+someNum = chosenArray{3}
