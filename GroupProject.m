6866aa69dbbf39650b3b2ca0ddece7a300f58e66
HappyFeet <jbulldogs55@gmail.com>
2020-11-03 10:22:27 -0600

    Update GroupProject.m

--- GroupProject.m
+++ GroupProject.m
@@ -3,3 +3,5 @@ clc
 materials = {'Concrete','Wood','Adobe'};
 Menu = menu('Select Material', materials);
 array1 = {'Concrete', 16, 30,96000,900,5,5;'Wood',23,53,115000,800,12,11;'Adobe',18,42,68000,600,6,5};
+
+chosenArray = array1(menu,:);
