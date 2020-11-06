2aaf5a4cf536c25bef66aaa22d40941a7fb3d8ce
ekp0281 <73851236+ekp0281@users.noreply.github.com>
2020-11-06 13:35:29 -0600

    Update P1_11_THUR_3.m

--- P1_11_THUR_3.m
+++ P1_11_THUR_3.m
@@ -5,51 +5,58 @@
 %Carl Raymundo
 %Emmanuel Pembele
 
+clear; close all; clc
 
-clear 
-clc
-close all
-
+% Create Cell Array by making a menu of the material choices
 materials = {'Concrete','Wood','Adobe'};
 Menu = menu('Select Material', materials);
 array1 = {'Concrete', 16, 30,96000,900,5,5;'Wood',23,53,115000,800,12,11;'Adobe',18,42,68000,600,6,5};
 
+% Calculate the cost of User's selected product
 chosenArray = array1(Menu,:);
 
-thicknessFt = chosenArray{2}/12;
+thicknessFt = chosenArray{2}/12;  %ft
 
-surfaceArea = 3000;
+surfaceArea = 3000; %ft
 
-materialCost = thicknessFt*surfaceArea*chosenArray{3};
+materialCost = thicknessFt*surfaceArea*chosenArray{3};   %$/ft^3
 
-miscCost = chosenArray{4};
+miscCost = chosenArray{4};     %$
 
-fixedLaborCost = chosenArray{5}*chosenArray{6}*chosenArray{7};
+fixedLaborCost = chosenArray{5}*chosenArray{6}*chosenArray{7};     %$
 
-fixedCost = materialCost + miscCost + fixedLaborCost;
+fixedCost = materialCost + miscCost + fixedLaborCost;     %$
 
+% Ask the User to input various details
 
+%Energy Cost per week
 energyCost = input('What is the energy cost per week(USD)? ');
 
+%Labor Cost per week
 laborCost = input('What is the labor cost per week(USD)? ');
 
+%Maintenance Cost per week
 maintenanceCost = input('What is the maintenance cost per week(USD)? ');
 
+%Landfill Cost per week
 landFillCost = input('What is the landfill cost per week(USD)? ');
 
-%number of weeks per year for zoo operation
+%Number of Weeks Operating 
 numWeeksPerYearOp = input('What is the number of weeks per year that the zoo will operate? ');
 
-%number of years of operation
+%Number of Years Operating
 yearOp = input('What is the number of years that the zoo will operate for? ');
 
-%Price of Admission
+%Price of Admission per person
 priceAdmission=input('What is the price of admission per person? ');
+
 %Number of People that visit per week
 visitor=input('What is the number of people that visit per week? ');
+
 %Expected Donations per week
 donations=input('What is the expected amount of donations per week? ');
 
+%Calculate the Breakeven Point
 weeklyIncome = priceAdmission*visitor+donations;
 
 capital = fixedCost;
@@ -64,14 +71,17 @@ costPerYear = costPerWeek * numWeeksPerYearOp;
 
 totalProfit = (revenuePerYear - costPerYear) * yearOp  - capital;
 
+%Calulate the Needed Donation for a Breakeven Point for 7 months
 donation = capital + (costPerWeek - weeklyIncome) * 28;
 
 breakEvenYear = (capital/(weeklyIncome - costPerWeek))/numWeeksPerYearOp;
 
+%Output Statement
 fprintf('\nMaterial: %s\n\tOperating %0.0f weeks per year will generate per year:\n\t\tRevenue:\t$%0.0f\n\t\tCost:\t\t$%0.0f\n\tThe breakeven time is %0.2f months\n\tThe total profit after %0.0f years is $%e.\n', materials{Menu}, numWeeksPerYearOp, revenuePerYear, costPerYear, monthsBreakeven, yearOp, totalProfit);
 
 fprintf('\nIt will take a one-time donation of $%0.2f to breakeven in seven months.\n', donation);
 
+%Create the Plot
 %x is years
 x = 0:yearOp
 
